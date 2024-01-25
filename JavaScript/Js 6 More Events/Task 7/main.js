888f698bcaf78c60eecd905a414ddb95fe1562ae
YaroslavHerhil <yarik.342.ua.itstep@gmail.com>
2024-01-25 20:51:39 +0100

    Added stUFFFFFFF

---
+++ JavaScript/Js 6 More Events/Task 7/main.js
@@ -0,0 +1,3 @@
+let my_cell = document.getElementById("cell");http://127.0.0.1:3000/JavaScript/Js%206%20More%20Events/Task%207/task7.html
+
+my_cell.AddEventListener('click', (event) => {console.log("Hey!")})
